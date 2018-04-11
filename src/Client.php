4b894e5fa4dc5aa59ab9e400a60c5e69700f7e57
Matthew Radford <mradford@protonmail.com>
2018-04-11 14:28:26 -0700

    Add client and request classes

---
+++ src/Client.php
@@ -0,0 +1,10 @@
+<?php namespace MattyRad\NounProject;
+
+use GuzzleHttp\Client as GuzzleClient;
+
+class Client
+{
+    public function send(Request $request)
+    {
+    }
+}
---
+++ src/Request.php
@@ -0,0 +1,5 @@
+<?php namespace MattyRad\NounProject;
+
+abstract class Request
+{
+}
